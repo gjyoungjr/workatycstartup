7125e8628fb1a5a181607426b45fcad8a1621ada
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-03-11 20:13:16 -0600

    change web page for scraper yc launch page -> yc job listing page

--- scraper.py
+++ scraper.py
@@ -22,14 +22,19 @@ service = Service(webdriver_path)
 # Launch Chrome browser
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
+yc_launch_page = "https://www.ycombinator.com/launches"
+yc_jobs_page = "https://www.workatastartup.com/jobs"
+
 # Wait for the page to load
-driver.get('https://www.ycombinator.com/launches')
+driver.get(yc_jobs_page)
 wait = WebDriverWait(driver, 10)
 
 # Store the HTML of all posts
 launch_cards_html = []
 num_posts = 0
 
+# def get_company_founders(): 
+    
 while True:
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     try:
@@ -40,7 +45,8 @@ while True:
     time.sleep(random.randint(2, 5)) 
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    launch_cards = soup.find_all('div', class_='post row align-center')
+    launch_cards = soup.find_all('div', class_='w-full bg-beige-lighter mb-2 rounded-md p-2 border border-gray-200 flex')
+    print(len(launch_cards))
 
     # Only add new launch cards that were not already in the list
     for card in launch_cards[num_posts:]:
@@ -61,28 +67,34 @@ for html in launch_cards_html:
     post = {}
 
     # Extract company name
-    company_name = soup.find('a', class_='post-company-name').text.strip()
+    company_name = soup.find('span', class_='font-bold').text.strip()
     post['company_name'] = company_name
 
-    # Extract description
-    description = soup.find('a', class_='post-tagline').text.strip()
+    # # Extract description
+    description = soup.find('span', class_='text-gray-600 block sm:inline').text.strip()
     post['description'] = description
+    
+    
+    job_name = soup.find('a', class_='font-bold captialize mr-5').text.strip()
+    post['job_name'] = job_name
+    
+    
 
-    # Extract post date
-    post_date = soup.find('div', class_='post-date').find('time')['datetime']
-    post['post_date'] = post_date
+    # # Extract post date
+    # post_date = soup.find('div', class_='post-date').find('time')['datetime']
+    # post['post_date'] = post_date
 
-    # Extract cohort
-    cohort = soup.find('div', class_='batch-tag post-tag').text.strip()
-    post['cohort'] = cohort
+    # # Extract cohort
+    # cohort = soup.find('div', class_='batch-tag post-tag').text.strip()
+    # post['cohort'] = cohort
 
-    # Extract tags
-    tags = [tag.text.strip() for tag in soup.find_all('div', class_='post-tag')]
-    post['tags'] = tags
+    # # Extract tags
+    # tags = [tag.text.strip() for tag in soup.find_all('div', class_='post-tag')]
+    # post['tags'] = tags
 
-    # Extract link
-    link = 'https://www.ycombinator.com' + soup.find('a', class_='post-title')['href']
-    post['link'] = link
+    # # Extract link
+    # link = 'https://www.ycombinator.com' + soup.find('a', class_='post-title')['href']
+    # post['link'] = link
 
     # Convert to JSON
     json_data = json.dumps(post, indent=2)
