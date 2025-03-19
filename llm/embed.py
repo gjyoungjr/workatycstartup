79c7375ca43010d4d959ce63344b8bc1ca9427a5
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-03-19 15:25:38 -0600

    add function to generate langchain documents

--- llm/embed.py
+++ llm/embed.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveJsonSplitter
 from typing import Dict, List
 import json 
 from  pprint import pprint
+from utils.helpers import clean_document_text
 
 def load_json_data(file_path: str) -> List[Dict]:
     """
@@ -31,11 +32,25 @@ def generate_embeddings(file_path: str):
     data = load_json_data(file_path=file_path)
     splitter = RecursiveJsonSplitter(max_chunk_size=300)
     documents = []
-
+   
     for item in data:
-      json_chunks = splitter.split_json(json_data=item)
-      pprint(json_chunks)
+        docs = splitter.create_documents(
+            texts=[{'job_description_detailed': clean_document_text(item['job_description_detailed'])}],
+            metadatas=[{
+                'company_name': item['company_name'],
+                'company_link': item['company_link'],
+                'company_description': item['description'],
+                'batch': item['batch'],
+                'source': item['job_link'],
+                'job_name': item['job_name'],
+                'location': item['location'],
+                'job_type': item['job_type'],
+                'tech_stack': item['tech_stack'],
+                'founders': item['founders'],
+            }]
+            )
+        documents.extend(docs)
+        
+    pprint(f"Documents : {documents[0]}")
         
         
-        
-    # pprint(documents)
