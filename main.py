bc67e26036fd5b5e47dc74d5fdb22e43a4e29338
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-03-23 12:56:17 -0600

    ran script to upsert docs into pinecone

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from web_scraper.scrapers import scrape_job_listings, scrape_company_founders, scrape_job_descriptions, parse_job_card
 from utils.helpers import save_to_json, clean_document_text
-from llm.embed import generate_embeddings
+from llm.vector_db import upsert_documents, create_documents, init_vector_store, load_pinecone_index
 from pprint import pprint
-
 from tqdm import tqdm
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 YC_JOB_LISTING_URL = "https://www.workatastartup.com/jobs"
 
 def main():
-   embeddings = generate_embeddings(file_path='yc_job_listings.json')
-   
-   
-   pprint(embeddings)
+    pinecone_index = load_pinecone_index(index_name=os.environ['PINECONE_INDEX_NAME'])
+    vector_store = init_vector_store(index=pinecone_index, embed_model='sentence-transformers/all-mpnet-base-v2')
+    docs = create_documents(file_path='yc_job_listings.json')
+    
+    upsert_documents(documents=docs, vector_store=vector_store)
+    
     # job_cards_html = scrape_job_listings(YC_JOB_LISTING_URL)
     
     # job_listings = []
@@ -21,10 +26,7 @@ def main():
     #     job_listings.append(job_listing)    
         
     # job_listings = scrape_company_founders(job_listings)
-    # job_listings = scrape_job_descriptions(job_listings) 
-    
-    # job_listings = [clean_document_text(job['job_description_detailed']) for job in job_listings]
-    
+    # job_listings = scrape_job_descriptions(job_listings)     
     # save_to_json(job_listings)  
   
 
