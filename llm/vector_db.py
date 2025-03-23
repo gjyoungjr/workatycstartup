1753ebb1b50039bab05e6340c116b234a23c402a
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-03-23 12:55:52 -0600

    add pinecone db functions

--- llm/vector_db.py
+++ llm/vector_db.py
@@ -7,11 +7,18 @@ from uuid import uuid4
 from langchain_text_splitters import RecursiveJsonSplitter
 from typing import  List
 from  pprint import pprint
-from utils.helpers import clean_document_text, load_json_data
+from utils.helpers import clean_document_text, load_json_data, transform_founders_data
+from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
 pinecone = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
+
+def init_vector_store(index, embed_model: str): 
+    embeddings = HuggingFaceEmbeddings(model_name=embed_model)
+    
+    return PineconeVectorStore(index=index, embedding=embeddings)
 
 def load_pinecone_index(index_name:str): 
     existing_indexes = [index_info["name"] for index_info in pinecone.list_indexes()]
@@ -19,7 +26,7 @@ def load_pinecone_index(index_name:str):
     if index_name not in existing_indexes:
         pinecone.create_index(
             name=index_name,
-            dimension=3072,
+            dimension=768,
             metric="cosine",
             spec=ServerlessSpec(cloud="aws", region="us-east-1"),
         )
@@ -28,11 +35,12 @@ def load_pinecone_index(index_name:str):
 
     return pinecone.Index(index_name)
     
-def upsert_documents(documents: List[Document]):
+def upsert_documents(documents: List[Document], vector_store: PineconeVectorStore):
     uuids = [str(uuid4()) for _ in range(len(documents))]
-    pprint(uuids)
+    vector_store.add_documents(documents=documents, ids=uuids)
+                
     
-def create_documents(file_path: str):
+def create_documents(file_path: str ):
     data = load_json_data(file_path=file_path)
     splitter = RecursiveJsonSplitter(max_chunk_size=300)
     documents = []
@@ -50,10 +58,10 @@ def create_documents(file_path: str):
                 'location': item['location'],
                 'job_type': item['job_type'],
                 'tech_stack': item['tech_stack'],
-                'founders': item['founders'],
+                'founders': transform_founders_data(item['founders']),
             }]
             )
         documents.extend(docs)
         
     return documents
-                
+
